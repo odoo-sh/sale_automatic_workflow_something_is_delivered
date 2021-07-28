45c939100b65ca8edcd5d54c26b58a030df269af
Abhishek <abhishek@sodexis.com>
2021-07-28 18:15:47 +0530

    [ID:20577]Add Something is Delivered Field to SO list view filter:

--- sale_automatic_workflow_something_is_delivered/models/sale.py
+++ sale_automatic_workflow_something_is_delivered/models/sale.py
@@ -7,7 +7,16 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
-    something_is_delivered = fields.Boolean(compute='_compute_something_is_delivered')
+    something_is_delivered = fields.Boolean(compute='_compute_something_is_delivered',search="_search_something_is_delivered")
+
+    
+    def _search_something_is_delivered(self, operator, value):
+        order = False
+        if operator == '=':
+            orders = self.search([]).filtered(lambda x : x.something_is_delivered == value )
+        else:
+            orders = self.search([]).filtered(lambda x : x.something_is_delivered == False )
+        return [('id', operator, [x.id for x in orders] if orders else False )]
 
     @api.depends('state', 'order_line', 'order_line.qty_delivered')
     def _compute_something_is_delivered(self):
